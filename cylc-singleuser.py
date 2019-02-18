0bfb243e4d14d0b716637ed9d17691bf6eb52e57
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-02-18 23:42:59 +1300

    Few minor improvements to remove exception when stopping app

--- cylc-singleuser.py
+++ cylc-singleuser.py
@@ -1,6 +1,8 @@
 import tornado.ioloop
 import tornado.web
 import sys
+import signal
+import logging
 
 
 class MainHandler(tornado.web.RequestHandler):
@@ -9,19 +11,42 @@ class MainHandler(tornado.web.RequestHandler):
         # At this point we would have Cylc running, so we just ned to have
         # a handler that gives the user's Dashboard... this acts now as
         # the entry point, in the same way that the GUI gcylc was before...
-        self.write("Hello, world " + username + "!")
+        self.write(f"""
+        <p>Hello, world {username} !</p>
+        <p>Click <a href="/hub/home">here</a> to go back to the hub... it is because
+        I am templeteless for now (a good joke, c'mon!)        
+        """)
+
+
+class MyApplication(tornado.web.Application):
+    is_closing = False
+
+    def signal_handler(self, signum, frame):
+        logging.info('exiting...')
+        self.is_closing = True
+
+    def try_exit(self):
+        if self.is_closing:
+            # clean up here
+            tornado.ioloop.IOLoop.instance().stop()
+            logging.info('exit success')
 
 
 def make_app():
-    return tornado.web.Application([
+    return MyApplication([
         (r"/user/(.*)", MainHandler),
     ])
 
 
 if __name__ == "__main__":
     app = make_app()
+    signal.signal(signal.SIGINT, app.signal_handler)
     if len(sys.argv) > 1:
         app.listen(int(sys.argv[1]))
     else:
         app.listen(8888)
-    tornado.ioloop.IOLoop.current().start()
+    tornado.ioloop.PeriodicCallback(app.try_exit, 100).start()
+    try:
+        tornado.ioloop.IOLoop.current().start()
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop()
