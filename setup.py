94f220327c93264886b86caac43b638218554ceb
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-26 16:09:55 +1300

    Add project minimal setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='cylc-jupyterhub',
+    version='1.0',
+    description='Cylc JupyterHub project, providing a UI server to Cylc',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cylc/cylc-jupyterhub/',
+    py_modules=['cylc_singleuser'],
+    python_requires='>=3.6',
+    install_requires=[
+        'jupyterhub',
+        'tornado'
+    ],
+    entry_points={
+        'console_scripts': [
+            'cylc-singleuser=cylc_singleuser:main'
+        ]
+    }
+)
