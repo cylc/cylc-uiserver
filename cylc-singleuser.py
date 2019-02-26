facd8fa1b2a519330a01a4d47101332bafcea6d3
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-26 13:58:31 +1300

    Remove static content

--- cylc-singleuser.py
+++ cylc-singleuser.py
@@ -47,6 +47,9 @@ class MainHandler(HubOAuthenticated, web.RequestHandler):
 class UserProfileHandler(HubOAuthenticated, web.RequestHandler):
 
     def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
+        self.setl_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
         self.set_header("Content-Type", 'application/json')
 
     @web.authenticated
