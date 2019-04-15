d3c9ef042119b7ca6f6b71485a7127689c344f59
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-04-16 10:01:48 +1200

    Add extras_require array in order to install test dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,32 @@
-from setuptools import setup
 from os import path
+
+from setuptools import setup
 
 here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+install_requires = [
+    'jupyterhub',
+    'tornado',
+    'graphene-tornado'
+]
+
+setup_requires = [
+    'pytest-runner'
+]
+
+tests_require = [
+    'pytest',
+    'coverage',
+    'pytest-cov'
+]
+
+extras_require = {
+    'tests': tests_require,
+    'all': install_requires + tests_require
+}
 
 setup(
     name='cylc-uiserver',
@@ -15,19 +37,10 @@ setup(
     url='https://github.com/cylc/cylc-uiserver/',
     py_modules=['handlers', 'cylc_singleuser'],
     python_requires='>=3.7',
-    install_requires=[
-        'jupyterhub',
-        'tornado',
-        'graphene-tornado'
-    ],
-    setup_requires=[
-        'pytest-runner'
-    ],
-    tests_require=[
-        'pytest',
-        'coverage',
-        'pytest-cov'
-    ],
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    extras_require=extras_require,
     entry_points={
         'console_scripts': [
             'cylc-singleuser=cylc_singleuser:main'
