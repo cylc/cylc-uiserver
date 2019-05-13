968f05238d7796bec3907230a24c313375b2081d
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-05-14 10:18:44 +1200

    Fix line limit

--- cylc_singleuser.py
+++ cylc_singleuser.py
@@ -62,34 +62,55 @@ class CylcUIServer(object):
             static_path=self._static,
             debug=True,
             handlers=[
-                (rf"{self._jupyter_hub_service_prefix}(.*.(css|js))", web.StaticFileHandler, {"path": self._static}),
-                (rf"{self._jupyter_hub_service_prefix}((fonts|img)/.*)", web.StaticFileHandler, {"path": self._static}),
-                (rf"{self._jupyter_hub_service_prefix}(favicon.png)", web.StaticFileHandler, {"path": self._static}),
+                (rf"{self._jupyter_hub_service_prefix}(.*.(css|js))",
+                 web.StaticFileHandler, {"path": self._static}),
+                (rf"{self._jupyter_hub_service_prefix}((fonts|img)/.*)",
+                 web.StaticFileHandler, {"path": self._static}),
+                (rf"{self._jupyter_hub_service_prefix}(favicon.png)",
+                 web.StaticFileHandler, {"path": self._static}),
 
-                (r"/(.*.(css|js))", web.StaticFileHandler, {"path": self._static}),
-                (r"/((fonts|img)/.*)", web.StaticFileHandler, {"path": self._static}),
-                (r"/(favicon.png)", web.StaticFileHandler, {"path": self._static}),
+                (r"/(.*.(css|js))",
+                 web.StaticFileHandler, {"path": self._static}),
+                (r"/((fonts|img)/.*)",
+                 web.StaticFileHandler, {"path": self._static}),
+                (r"/(favicon.png)",
+                 web.StaticFileHandler, {"path": self._static}),
 
-                (url_path_join(self._jupyter_hub_service_prefix, 'oauth_callback'), HubOAuthCallbackHandler),
-                (url_path_join(self._jupyter_hub_service_prefix, 'userprofile'), UserProfileHandler),
-                (url_path_join(self._jupyter_hub_service_prefix, 'suites'), CylcScanHandler),
+                (url_path_join(
+                    self._jupyter_hub_service_prefix, 'oauth_callback'),
+                    HubOAuthCallbackHandler),
+                (url_path_join(
+                    self._jupyter_hub_service_prefix, 'userprofile'),
+                    UserProfileHandler),
+                (url_path_join(
+                    self._jupyter_hub_service_prefix, 'suites'),
+                    CylcScanHandler),
 
-                (self._jupyter_hub_service_prefix, MainHandler, {"path": self._static}),
+                (self._jupyter_hub_service_prefix,
+                    MainHandler, {"path": self._static}),
 
                 # graphql
-                (url_path_join(self._jupyter_hub_service_prefix, '/graphql'),
-                 TornadoGraphQLHandler, dict(graphiql=True, schema=schema)),
-                (url_path_join(self._jupyter_hub_service_prefix, '/graphql/batch'),
-                 TornadoGraphQLHandler, dict(graphiql=True, schema=schema, batch=True)),
-                (url_path_join(self._jupyter_hub_service_prefix, '/graphql/graphiql'),
-                 TornadoGraphQLHandler, dict(graphiql=True, schema=schema)),
+                (url_path_join(self._jupyter_hub_service_prefix,
+                               '/graphql'),
+                    TornadoGraphQLHandler,
+                    dict(graphiql=True, schema=schema)),
+                (url_path_join(self._jupyter_hub_service_prefix,
+                               '/graphql/batch'),
+                    TornadoGraphQLHandler,
+                    dict(graphiql=True, schema=schema, batch=True)),
+                (url_path_join(self._jupyter_hub_service_prefix,
+                               '/graphql/graphiql'),
+                    TornadoGraphQLHandler,
+                    dict(graphiql=True, schema=schema)),
 
-                # since jupyterhub 1.0.0, the "My Server link the hub page removed the last
-                # slash from the URL. This handler is based on the handler with the same name
-                # in jupyterhub 1.0.0, and adds back the last `/` to every URL in the app.
+                # since jupyterhub 1.0.0, the "My Server link the hub page
+                # removed the last slash from the URL. This handler is based
+                # on the handler with the same name in jupyterhub 1.0.0,
+                # and adds back the last `/` to every URL in the app.
                 (r".*", AddSlashHandler)
             ],
-            # FIXME: decide (and document) whether cookies will be permanent after server restart.
+            # FIXME: decide (and document) whether cookies will be permanent
+            # after server restart.
             cookie_secret="cylc-secret-cookie"
         )
 
@@ -110,13 +131,20 @@ def main():
     )
     parser.add_argument('-p', '--port', action="store", dest="port", type=int,
                         default=8888)
-    parser.add_argument('-s', '--static', action="store", dest="static", required=True)
+    parser.add_argument('-s', '--static', action="store", dest="static",
+                        required=True)
     args = parser.parse_args()
 
-    jupyterhub_service_prefix = os.environ.get('JUPYTERHUB_SERVICE_PREFIX', '/')
+    jupyterhub_service_prefix = os.environ.get(
+        'JUPYTERHUB_SERVICE_PREFIX', '/')
     logging.info(f"JupyterHub Service Prefix: {jupyterhub_service_prefix}")
-    ui_server = CylcUIServer(port=args.port, static=args.static, jupyter_hub_service_prefix=jupyterhub_service_prefix)
-    logging.info(f"Listening on {args.port} and serving static content from {args.static}")
+    ui_server = CylcUIServer(
+        port=args.port,
+        static=args.static,
+        jupyter_hub_service_prefix=jupyterhub_service_prefix
+    )
+    logging.info(f"Listening on {args.port} and serving static content from "
+                 f"{args.static}")
 
     logging.info("Starting Cylc UI")
     ui_server.start()
@@ -126,4 +154,5 @@ if __name__ == "__main__":
     main()
 
 
-__all__ = ['MainHandler', 'UserProfileHandler', 'MyApplication', 'CylcUIServer', 'main']
+__all__ = ['MainHandler', 'UserProfileHandler', 'MyApplication',
+           'CylcUIServer', 'main']
