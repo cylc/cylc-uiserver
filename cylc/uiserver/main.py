6440805de953f3cbd4f73a14129b62a561d7d87c
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-09-09 14:33:18 +1200

    Add argparse option for debug (for Tornado app)

--- cylc/uiserver/main.py
+++ cylc/uiserver/main.py
@@ -62,12 +62,16 @@ class CylcUIServer(object):
             self.data_mgr.data,
             ws_mgr=self.ws_mgr)
 
-    def _make_app(self):
-        """Crete a Tornado web application."""
+    def _make_app(self, debug: bool):
+        """Crete a Tornado web application.
+
+        Args:
+            debug (bool): flag to set debugging in the Tornado application
+        """
         logging.info(self._static)
         return MyApplication(
             static_path=self._static,
-            debug=True,
+            debug=debug,
             handlers=[
                 (rf"{self._jupyter_hub_service_prefix}(.*.(css|js))",
                  web.StaticFileHandler, {"path": self._static}),
@@ -116,8 +120,8 @@ class CylcUIServer(object):
             cookie_secret="cylc-secret-cookie"
         )
 
-    def start(self):
-        app = self._make_app()
+    def start(self, debug: bool):
+        app = self._make_app(debug)
         signal.signal(signal.SIGINT, app.signal_handler)
         app.listen(self._port)
         ioloop.PeriodicCallback(app.try_exit, 100).start()
@@ -142,6 +146,8 @@ def main():
                         default=8888)
     parser.add_argument('-s', '--static', action="store", dest="static",
                         required=True)
+    parser.add_argument('--debug', action="store_true", dest="debug",
+                        default=False)
     args = parser.parse_known_args()[0]
 
     jupyterhub_service_prefix = os.environ.get(
@@ -156,7 +162,7 @@ def main():
                  f"{args.static}")
 
     logging.info("Starting Cylc UI")
-    ui_server.start()
+    ui_server.start(args.debug)
 
 
 if __name__ == "__main__":
