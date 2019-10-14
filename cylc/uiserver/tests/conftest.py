f8d0c38f46bf07b7fdc5274521118c6a0322c80d
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-10-14 14:49:35 +1300

    Add a fixture ZMQClient for tests

---
+++ cylc/uiserver/tests/conftest.py
@@ -0,0 +1,46 @@
+# Copyright (C) 2019 NIWA & British Crown (Met Office) & Contributors.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Test code and fixtures."""
+
+import inspect
+
+import pytest
+
+from cylc.flow.network.client import ZMQClient
+
+
+class TestAsyncClient(ZMQClient):
+    host = ''
+    port = 0
+    encode_method = None
+    decode_method = None
+    secret_method = None
+
+    def __init__(self):
+        self.returns = None
+
+    def will_return(self, returns):
+        self.returns = returns
+
+    async def async_request(self, command, args=None, timeout=None):
+        if inspect.isclass(self.returns) and \
+                issubclass(self.returns, Exception):
+            raise self.returns
+        return self.returns
+
+
+@pytest.fixture
+def async_client():
+    return TestAsyncClient()
