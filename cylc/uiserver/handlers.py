f57776828e93951db9b2104588292e898c3295b5
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-05-14 11:54:18 +1200

    Remove Tornado from server header

--- cylc/uiserver/handlers.py
+++ cylc/uiserver/handlers.py
@@ -36,6 +36,7 @@ class BaseHandler(web.RequestHandler):
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Server', '')
 
 
 class APIHandler(BaseHandler):
