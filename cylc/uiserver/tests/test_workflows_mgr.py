83b9b174ddb47302325b1e909c9475579d4a97e3
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-09-01 15:45:33 +0100

    test states and fix data purging

--- cylc/uiserver/tests/test_workflows_mgr.py
+++ cylc/uiserver/tests/test_workflows_mgr.py
@@ -13,12 +13,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from itertools import product
+from pathlib import Path
 import pytest
+from random import random
 
 from pytest_mock import MockFixture
 
-from cylc.uiserver.workflows_mgr import *
+from cylc.flow import ID_DELIM
 from cylc.flow.exceptions import ClientTimeout
+from cylc.flow.network import API
+from cylc.flow.suite_files import (
+    ContactFileFields as CFF,
+    SuiteFiles
+)
+
+from cylc.uiserver.workflows_mgr import *
 
 from .conftest import AsyncClientFixture
 
@@ -141,14 +151,110 @@ async def test_est_workflow(
 # --- WorkflowsManager
 
 
-def test_workflows_manager_constructor():
-    mgr = WorkflowsManager(None)
-    assert not mgr.workflows
-
-
 def test_workflows_manager_spawn_workflow():
     mgr = WorkflowsManager(None)
     mgr.spawn_workflow()
-    assert not mgr.workflows
+    assert not mgr.active
 
 # TODO: add tests for remaining methods in WorkflowsManager
+
+
+def mk_flow(path, reg, active=True):
+    """Make a workflow appear on the filesystem for scan purposes.
+
+    Args:
+        path (pathlib.Path):
+            The directory to create the mocked workflow in.
+        reg (str):
+            The registered name for this workflow.
+        active (bool):
+            If True then a contact file will be written.
+
+    """
+    run_dir = path / reg
+    srv_dir = run_dir / SuiteFiles.Service.DIRNAME
+    contact = srv_dir / SuiteFiles.Service.CONTACT
+    fconfig = run_dir / SuiteFiles.FLOW_FILE
+    run_dir.mkdir()
+    fconfig.touch()  # cylc uses this to identify a dir as a workflow
+    srv_dir.mkdir()
+    if active:
+        with open(contact, 'w+') as contact_file:
+            contact_file.write(
+                '\n'.join([
+                    f'{CFF.API}={API}',
+                    f'{CFF.HOST}=42',
+                    f'{CFF.PORT}=42',
+                    f'{CFF.UUID}=42'
+                ])
+            )
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    # generate all possile state changes
+    'active_before,active_after',
+    product(['active', 'inactive', None], repeat=2)
+)
+async def test_workflow_state_changes(tmp_path, active_before, active_after):
+    """It correctly identifies workflow state changes from the filesystem."""
+    tmp_path /= str(random())
+    tmp_path.mkdir()
+
+    # mock the results of the previous scan
+    wfm = WorkflowsManager(None, context=None, run_dir=tmp_path)
+    wid = f'{wfm.owner}{ID_DELIM}a'
+    if active_before == 'active':
+        wfm.active[wid] = {
+            CFF.API: API,
+            CFF.UUID: '42'
+        }
+    elif active_before == 'inactive':
+        wfm.inactive.add(wid)
+
+    # mock the filesystem in the new state
+    if active_after == 'active':
+        mk_flow(tmp_path, 'a', active=True)
+    if active_after == 'inactive':
+        mk_flow(tmp_path, 'a', active=False)
+
+    # see what state changes the workflow manager detects
+    changes = []
+    async for change in wfm._workflow_state_changes():
+        changes.append(change)
+
+    # compare those changes to expectations
+    if active_before == active_after:
+        assert changes == []
+    else:
+        assert len(changes) == 1
+        assert (wid, active_before, active_after) == changes[0][:3]
+
+
+@pytest.mark.asyncio
+async def test_workflow_state_change_restart(tmp_path):
+    """It identifies workflows which have restarted between scans."""
+    # mock the result of the previous scan
+    wfm = WorkflowsManager(None, context=None, run_dir=tmp_path)
+    wid = f'{wfm.owner}{ID_DELIM}a'
+    wfm.active[wid] = {
+            CFF.API: API,
+            CFF.UUID: '41'
+    }
+
+    # create a new workflow with the same name but a different UUID
+    mk_flow(tmp_path, 'a', active=True)
+
+    # see what state changes the workflow manager detects
+    changes = []
+    async for change in wfm._workflow_state_changes():
+        changes.append(change)
+
+    # the flow should be marked as becomming inactive then active again
+    assert [change[:3] for change in changes] == [
+        (wid, 'active', 'inactive'),
+        (wid, 'inactive', 'active')
+    ]
+
+    # it should have picked up the new uuid too
+    assert changes[1][3][CFF.UUID] == '42'
