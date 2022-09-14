674702d73f565faf2dbfa38a9f9a5f1e4df0fcf0
Hilary James Oliver <hilary.j.oliver@gmail.com>
2022-09-15 10:42:27 +1200

    Change to @minrk's suggestion for JupyterHub bug workaround.

--- cylc/uiserver/scripts/hub.py
+++ cylc/uiserver/scripts/hub.py
@@ -18,56 +18,16 @@ Launch the Cylc hub for running the Cylc Web GUI.
 """
 
 import os
+import sys
+from unittest.mock import patch
 from pathlib import Path
-from functools import partial
 
 from jupyterhub.app import JupyterHub
-from tornado.ioloop import IOLoop
 
 from cylc.uiserver import (
     __version__,
     __file__ as uis_pkg
 )
-
-
-def launch_instance(cls, argv=None):
-    """JupyterHub class method to correctly pass argv to launch_instance_async.
-
-    At JupyterHub 3.0.0 this incorrectly passes our config file path as second
-    arg of Tornado run_sync, which is supposed to be a timeout value.
-
-    Acknowledgment:
-       Code derived from the JupyterHub source (revised BSD license):
-       https://github.com/jupyterhub/jupyterhub/blob/main/jupyterhub/app.py
-       | Copyright (c) Jupyter Development Team.
-       | Distributed under the terms of the Modified BSD License.
-
-    TODO: this can be removed once cylc-uiserver depends on jupyter >= 3.0.1
-    Bug fix:
-        https://github.com/jupyterhub/jupyterhub/pull/4039
-    Reviewer: Will do a 3.0.1 pretty soon after some more 3.0 feedback.
-
-    """
-    self = cls.instance()
-    self._init_asyncio_patch()
-    loop = IOLoop(make_current=False)
-    try:
-        # bug: loop.run_sync(self.launch_instance_async, argv)
-        loop.run_sync(partial(self.launch_instance_async, argv))
-    except Exception:
-        loop.close()
-        raise
-    try:
-        loop.start()
-    except KeyboardInterrupt:
-        print("\nInterrupted")
-    finally:
-        loop.stop()
-        loop.close()
-
-
-# Patch JupyterHub to use our bug-fix version of launch_instance.
-JupyterHub.launch_instance = classmethod(launch_instance)
 
 
 def main(*args):
@@ -80,6 +40,14 @@ def main(*args):
     # set an env var flag to help load the config
     os.environ['CYLC_HUB_VERSION'] = __version__
     try:
-        JupyterHub.launch_instance(args)
+        # JupyterHub 3.0.0 incorrectly passes our config file path as second
+        # arg of Tornado run_sync, which is supposed to be a timeout value.
+        #   https://github.com/jupyterhub/jupyterhub/pull/4039
+        # Reviewer: "Will do a 3.0.1 pretty soon after some more 3.0 feedback."
+        # TODO: once we depend on jupyter >= 3.0.1 can revert to this:
+        #   JupyterHub.launch_instance(args)
+        with patch.object(sys, "argv", [sys.argv[0]] + list(args)):
+            # Patch works via traitlets.config.Application.parse_command_line
+            JupyterHub.launch_instance()
     finally:
         del os.environ['CYLC_HUB_VERSION']
