2f8c291c4c973579706a824bf606dbb8da773520
Mel Hall <37735232+datamel@users.noreply.github.com>
2023-02-06 12:55:03 +0000

    update descriptions in schema

--- cylc/uiserver/schema.py
+++ cylc/uiserver/schema.py
@@ -98,7 +98,7 @@ class Play(graphene.Mutation):
     class Meta:
         description = sstrip('''
             Start, resume or restart a workflow run.
-+            Valid for: ['stopped', 'paused'] workflows.
+            Valid for: ['stopped', 'paused'] workflows.
         ''')
         resolver = partial(mutator, command='play')
 
