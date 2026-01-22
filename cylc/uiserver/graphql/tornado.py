91114ce1dd5f490149dc93c3e02d255587bc1955
David Sutherland <davidwollow@gmail.com>
2026-01-22 21:45:35 +1300

    error reason propagate

--- cylc/uiserver/graphql/tornado.py
+++ cylc/uiserver/graphql/tornado.py
@@ -59,11 +59,7 @@ from graphql.execution.middleware import MiddlewareManager
 from graphql.pyutils import is_awaitable
 from graphql.validation import validate
 
-from cylc.flow.network.graphql import (
-    NULL_VALUE,
-    instantiate_middleware,
-    strip_null
-)
+from cylc.flow.network.graphql import instantiate_middleware
 
 if TYPE_CHECKING:
     from graphene import Schema
@@ -71,20 +67,6 @@ if TYPE_CHECKING:
 
 MUTATION_ERRORS_FLAG = "graphene_mutation_has_errors"
 MAX_VALIDATION_ERRORS = None
-
-
-def data_search_action(data, action):
-    if isinstance(data, dict):
-        return {
-            key: data_search_action(val, action)
-            for key, val in data.items()
-        }
-    if isinstance(data, list):
-        return [
-            data_search_action(val, action)
-            for val in data
-        ]
-    return action(data)
 
 
 def get_content_type(request: 'HTTPServerRequest') -> str:
@@ -183,7 +165,11 @@ class TornadoGraphQLHandler(web.RequestHandler):
             else:
                 result, status_code = await self.get_response(data)
 
-            self.set_status(status_code)
+            if status_code == 400:
+                self.set_status(status_code, reason=result)
+            else:
+                self.set_status(status_code)
+
             self.set_header("Content-Type", "application/json")
             self.write(result)
             await self.finish()
@@ -192,7 +178,7 @@ class TornadoGraphQLHandler(web.RequestHandler):
             response = e.response
             response["Content-Type"] = "application/json"
             response.content = self.json_encode(
-                self.request, {"errors": [self.format_error(e)]}
+                {"errors": [self.format_error(e)]}
             )
             return response
 
@@ -232,39 +218,16 @@ class TornadoGraphQLHandler(web.RequestHandler):
             if self.batch:
                 response["id"] = _id
                 response["status"] = status_code
-            try:
-                result = self.json_encode(response)
-            except TypeError:
-                # Catch exceptions in response
-                errors = []
 
-                def exc_to_errors(data):
-                    if isinstance(data, Exception):
-                        errors.append({
-                            'message': (
-                                f'{data.value}'
-                                if hasattr(data, 'value') else f'{data}'
-                            )
-                        })
-                        return NULL_VALUE
-                    return data
-
-                response = data_search_action(
-                    response,
-                    exc_to_errors
-                )
-                response.setdefault("errors", []).extend(errors)
-                response = strip_null(response)
-
-                result = self.json_encode(response)
+            result = self.json_encode(response)
         else:
             result = None
 
         return result, status_code
 
-    def json_encode(self, d, pretty=False):
+    def json_encode(self, d):
         if (
-            (self.pretty or pretty)
+            self.pretty
             or self.get_query_argument("pretty", False)
             or self.get_body_argument("pretty", False)
         ):
